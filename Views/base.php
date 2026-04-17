79e007929fa0b9eb46aceb10d44e1e52dd15d3fb
noe-root <250055@site.asty-moulin.be>
2026-04-17 20:05:31 +0200

    Update base layout, styles, and DB connection

--- Views/base.php
+++ Views/base.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Project</title>
     <link rel="stylesheet" href="Assets/CSS/index.css">
+    <script src="Assets/JS/animations.js"></script>
 </head>
 <body class = "fond">
     <header>
