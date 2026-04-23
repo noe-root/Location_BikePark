473e6d304934a6e242f45f85f77d106bdc49a777
noe-root <250055@site.asty-moulin.be>
2026-04-23 09:34:35 +0200

    Update base layout, JS assets, and DB connection

--- Views/base.php
+++ Views/base.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Project</title>
     <link rel="stylesheet" href="Assets/CSS/index.css">
-    <script src="Assets/JS/animations.js"></script>
+    <script src="Assets/JS/animation.js"></script>
 </head>
 <body class = "fond">
     <header>
