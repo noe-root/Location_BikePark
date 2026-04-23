28305cf924ef8164ea11e4353170edf4a44aa829
noe-root <250055@site.asty-moulin.be>
2026-04-23 09:54:03 +0200

    feat(forms): add required fields to user forms

--- User/userConnection.php
+++ User/userConnection.php
@@ -2,11 +2,11 @@
 <form action="" method="post">
     <div class = "espace">
         <label for="adressMail">Adresse Mail</label>
-        <input type="email" name="adressMail" id="adressMail">
+        <input type="email" name="adressMail" id="adressMail" required>
     </div>
     <div class = "espace">
         <label for="password">Mot de passe</label>
-        <input type="password" name="password" id="password">
+        <input type="password" name="password" id="password" required>
     </div>
     <div class = "espace">
         <button type="submit" name="envoyer">Envoyer</button>
