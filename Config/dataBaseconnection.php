a23da7a9e82dbd4ab3d09703fe70950ada545362
noe-root <250055@site.asty-moulin.be>
2026-04-24 21:28:34 +0200

    Update DB config and add image asset

--- Config/dataBaseconnection.php
+++ Config/dataBaseconnection.php
@@ -1,7 +1,7 @@
 <?php
     try {
-        //$pdo = new PDO("mysql:host=192.168.68.51;dbname=sys;port=3306", "Noe", "root", [
-        $pdo = new PDO("mysql:host=10.10.31.122;dbname=sys;port=3306", "Noe", "root", [
+        $pdo = new PDO("mysql:host=192.168.68.59;dbname=sys;port=3306", "Noe", "root", [
+        //$pdo = new PDO("mysql:host=10.10.31.122;dbname=sys;port=3306", "Noe", "root", [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
         ]);
